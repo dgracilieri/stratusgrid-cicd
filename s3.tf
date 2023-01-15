3dafba64d6725176c4ec3095b4a3b914c486d871
Diego Gracilieri <root@DIEGO-GRACILIERI.local>
2023-01-15 14:22:23 -0500

    Poryecto CICD V2

--- s3.tf
+++ s3.tf
@@ -1,10 +1,13 @@
 resource "aws_s3_bucket" "s3bucket-cicd" {
-  bucket = "${var.env_name}${var.s3-bucket-name}"
-
-  
-
-  
+  bucket = "${var.env_name}${var.s3-bucket-name}" 
   tags = local.common_tags
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }   
 
  
 }
@@ -14,5 +17,6 @@ resource "aws_s3_bucket_acl" "acl" {
   acl = "private"
   
 }
+
 
 
